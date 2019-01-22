2b7c137f7ec575a163f1c85b75d36d5bb67ad6d3
SYNCROSOFT\florin_nica <florin13nica98@gmail.com>
2019-01-22 13:43:14 +0200

    Update project

--- src/main/java/ro/sync/json/instance/generator/JsonGeneratorEngine.java
+++ src/main/java/ro/sync/json/instance/generator/JsonGeneratorEngine.java
@@ -25,34 +25,18 @@ public class JsonGeneratorEngine {
 	public JsonGeneratorEngine(JsonGeneratorOptions options) {
 		this.options = options;
 	}
-	//TODO refactor
-	public int getRandomNumberBetween (Number min, Number max) {
-		int randomNumber = (min != null) ? (int) min : 0;
-		Random rand = new Random();
 
-		if (min != null && max != null) {
-			randomNumber = rand.nextInt((int) max - (int) min + 1) + (int)min;
-		}else if (min == null && max !=null) {
-			randomNumber = rand.nextInt((int) max);
-		}else if (min != null && max == null){
-			randomNumber = rand.nextInt(100 + (int)min) + (int)min;
-		}else {
-			randomNumber = rand.nextInt(10);
-		}
-		return randomNumber;
-	}
-	
 	public String generateRandomString(int length) {
     	final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        Random random = new Random();
         StringBuilder stringBuilder = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            stringBuilder.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+            stringBuilder.append(ALPHABET.charAt(new Random().nextInt(ALPHABET.length())));
         }
         return stringBuilder.toString();
     }
 	
     void visit(Schema schema) {
+    	System.out.println("VISITED SCHEMA: " + schema.getClass());
     	if (schema instanceof ObjectSchema) {
     		visitObjectSchema((ObjectSchema) schema);
     	} else if (schema instanceof NumberSchema) {
@@ -72,71 +56,6 @@ public class JsonGeneratorEngine {
     	} 
     	// TODO Handle all schemas.
     }
-    
-    /**
-     * Appends an integer value between minimum and maximum. If minimum and maximum are not defined, they are
-     * assigned values 0 and 100, respectively. 
-     * @param numberSchema schema to visit
-     */
-    void visitNumberSchema(NumberSchema numberSchema) {
-        visitMultipleOf(numberSchema.getMultipleOf());
-        
-        int minimum = (numberSchema.getMinimum() != null) ? numberSchema.getMinimum().intValue() : 0;
-        int maximum = (numberSchema.getMaximum() != null) ? numberSchema.getMaximum().intValue() : 100;
-        
-        int exclusiveMinimum = (numberSchema.isExclusiveMinimum() ? 1:0 );
-        int exclusiveMaximum = (numberSchema.isExclusiveMaximum() ? 1:0 );
-        
-        builder.append(" ").append(options.isGenerateRandomValues() ? getRandomNumberBetween(minimum + exclusiveMinimum, maximum - exclusiveMaximum) : minimum);
-    }
-    
-    /**
-     * Appends a random number of items if the option is random, or 2 items by default. The items use the lastPropertyName 
-     * as they have the same name + an increasing index.
-     * @param arraySchema schema to visit
-     */
-    void visitArraySchema(ArraySchema arraySchema) {
-    	// get minimum and maximum number of items the schema can contain
-        Number minimum = arraySchema.getMinItems() != null ? arraySchema.getMinItems() : 2;
-        Number maximum = arraySchema.getMaxItems() != null ? arraySchema.getMaxItems() : 10;
-        
-        String lastPropertyName = lastProperty;
-        int numberOfItems = options.isGenerateRandomValues() ? getRandomNumberBetween (minimum, maximum) : minimum.intValue();
-        
-        visitUniqueItems(arraySchema.needsUniqueItems());
-        builder.append("[");
-        
-        if(arraySchema.getAllItemSchema() != null) {
-        	for (int i = 0; i < numberOfItems; i++) {
-        		lastProperty = lastPropertyName + i;
-            	visit(arraySchema.getAllItemSchema());
-            	if (i < numberOfItems - 1) {
-            		builder.append(",");
-            	}
-            }
-        } else if (arraySchema.getItemSchemas() != null) {
-        	List<Schema> itemSchemas = arraySchema.getItemSchemas();
-        	for (int i = 0; i < itemSchemas.size(); ++i) {
-        		visit(itemSchemas.get(i));
-        		builder.append(",");
-        	}
-        }
-        if (builder.charAt(builder.length() - 1) == ',') {
-        	builder.setLength(builder.length()-1);
-        }
-        builder.append("]");
-   
-        visitSchemaOfAdditionalItems(arraySchema.getSchemaOfAdditionalItems());
-        //visitContainedItemSchema(arraySchema.getContainedItemSchema());
-    }
-    /**
-     * Visit the schema that is referred in the main schema definition.
-     * @param referenceSchema
-     */
-    void visitReferenceSchema(ReferenceSchema referenceSchema) {
-        visit(referenceSchema.getReferredSchema());
-    }
-
     /**
      * 
      * @param objectSchema schema to visit
@@ -165,6 +84,103 @@ public class JsonGeneratorEngine {
         builder.append("}");
     }
     /**
+     * Appends an integer value between minimum and maximum. If minimum and maximum are not defined, they are assigned
+     * values 0 and 100, respectively. If multipleOf is defined, then the integer value will be a multiple of that number.
+     * @param numberSchema schema to visit
+     */
+    void visitNumberSchema(NumberSchema numberSchema) {
+        System.out.println(numberSchema.getMultipleOf());
+        
+        int minimum = (numberSchema.getMinimum() != null) ? numberSchema.getMinimum().intValue() : 0;
+        int maximum = (numberSchema.getMaximum() != null) ? numberSchema.getMaximum().intValue() : 100;
+        
+        int exMinimum = (numberSchema.isExclusiveMinimum() ? 1:0 );
+        int exMaximum = (numberSchema.isExclusiveMaximum() ? 1:0 );
+        
+        int isMultipleOf = numberSchema.getMultipleOf() != null ? 1 : 0;
+        int multipleOf = isMultipleOf == 1 ? numberSchema.getMultipleOf().intValue() : 1;
+        
+        int lowerLimit = (minimum + exMinimum);
+        int upperLimit = (maximum + exMaximum);
+        
+        if (options.isGenerateRandomValues()) {
+        	builder.append(" ").append(new Random().nextInt(upperLimit) + lowerLimit/ multipleOf * multipleOf);
+        } else {
+        	builder.append(" ").append((lowerLimit/ multipleOf + isMultipleOf) * multipleOf);
+        }
+    }
+    
+    /**
+     * Appends a random number of items if the option is random, or 2 items by default. The items use the lastPropertyName 
+     * as they have the same name + an increasing index.
+     * @param arraySchema schema to visit
+     */
+    void visitArraySchema(ArraySchema arraySchema) {
+    	// get minimum and maximum number of items the schema can contain
+        int minItems = arraySchema.getMinItems() != null ? arraySchema.getMinItems().intValue() : 2;
+        int maxItems = arraySchema.getMaxItems() != null ? arraySchema.getMaxItems().intValue() : 10;
+        
+        String lastPropertyName = lastProperty;
+        int numberOfItems = options.isGenerateRandomValues() ? new Random().nextInt(maxItems) + minItems : minItems;
+        
+        visitUniqueItems(arraySchema.needsUniqueItems());
+        builder.append("[");
+        
+        if(arraySchema.getAllItemSchema() != null) {
+        	for (int i = 0; i < numberOfItems; i++) {
+        		lastProperty = lastPropertyName + i;
+            	visit(arraySchema.getAllItemSchema());
+            	if (i < numberOfItems - 1) {
+            		builder.append(",");
+            	}
+            }
+        } else if (arraySchema.getItemSchemas() != null) {
+        	List<Schema> itemSchemas = arraySchema.getItemSchemas();
+        	for (int i = 0; i < itemSchemas.size(); ++i) {
+        		visit(itemSchemas.get(i));
+        		builder.append(",");
+        	}
+        }
+        if (builder.charAt(builder.length() - 1) == ',') {
+        	builder.setLength(builder.length()-1);
+        }
+        builder.append("]");
+   
+        visitSchemaOfAdditionalItems(arraySchema.getSchemaOfAdditionalItems());
+        //visitContainedItemSchema(arraySchema.getContainedItemSchema());
+    }
+    /**
+     * Appends a random string element if the option is random, or the name of the property otherwise.
+     * @param stringSchema schema to visit
+     */
+    void visitStringSchema(StringSchema stringSchema) {
+    	// get minimum and maximum length the schema can have
+        int minLenght = stringSchema.getMinLength() != null ? stringSchema.getMinLength().intValue() : 2;
+        int maxLenght = stringSchema.getMaxLength() != null ? stringSchema.getMaxLength().intValue() : 10;
+        
+        builder.append(" \"");
+        // visit format if defined
+        if (stringSchema.getFormatValidator().formatName() != "unnamed-format") {
+        	visitFormat(stringSchema.getFormatValidator());
+        } else {
+        	builder.append(options.isGenerateRandomValues() ? generateRandomString(new Random().nextInt(maxLenght) + minLenght) : lastProperty);
+        }
+        builder.append("\"");
+        System.out.println(stringSchema.getPattern());
+        
+        // visit pattern if defined
+        if (stringSchema.getPattern() != null) {
+        	visitPattern(stringSchema.getPattern(), minLenght, maxLenght);
+        }
+    }
+    /**
+     * Visit the schema referred in the main schema definition.
+     * @param referenceSchema
+     */
+    void visitReferenceSchema(ReferenceSchema referenceSchema) {
+        visit(referenceSchema.getReferredSchema());
+    }
+    /**
      * Appends the property name and visits schema if not empty.
      * @param propertyName
      * @param schema
@@ -179,35 +195,6 @@ public class JsonGeneratorEngine {
     	}
     	builder.append(",");
     }
-    
-    /**
-     * Appends a random string element if the option is random, or the name of the property otherwise.
-     * @param stringSchema schema to visit
-     */
-    void visitStringSchema(StringSchema stringSchema) {
-    	// get minimum and maximum length the schema can have
-        Number minLenght = stringSchema.getMinLength() != null ? stringSchema.getMinLength() : 2;
-        Number maxLenght = stringSchema.getMaxLength() != null ? stringSchema.getMaxLength() : 10;
-        
-        builder.append(" \"");
-        
-        // visit format if defined
-        if (stringSchema.getFormatValidator().formatName() != "unnamed-format") {
-        	visitFormat(stringSchema.getFormatValidator());
-        } else {
-        	builder.append(options.isGenerateRandomValues() ? generateRandomString(getRandomNumberBetween(minLenght, maxLenght)) : lastProperty);
-        }
-        
-        builder.append("\"");
-        
-        System.out.println(stringSchema.getPattern());
-        
-        // visit pattern if defined
-        if (stringSchema.getPattern() != null) {
-        	visitPattern(stringSchema.getPattern(), minLenght, maxLenght);
-        }
-    }
-    
     /**
      * Appends a random element of the enumeration if the option is random, or the first element otherwise. 
      * @param enumSchema schema to visit
@@ -222,7 +209,23 @@ public class JsonGeneratorEngine {
 	    	}
     	}
     }
-
+    void visitCombinedSchema(CombinedSchema combinedSchema) {
+    	ValidationCriterion criterion = combinedSchema.getCriterion();
+		System.out.println("criterion: " +criterion);
+		Collection<Schema> subschemas = combinedSchema.getSubschemas();
+		System.out.println(subschemas.size());
+		
+		if (criterion == CombinedSchema.ALL_CRITERION) {
+			for (Schema subschema : subschemas) {
+				System.out.print(subschema.getClass() + " ");
+				if (subschema instanceof EnumSchema || subschema instanceof ReferenceSchema) {
+					visit(subschema);
+				}
+			}
+		} else {
+			visit(subschemas.iterator().next());
+		}
+    }
     void visitSchemaDependency(String propKey, Schema schema) {
     }
 
@@ -296,24 +299,6 @@ public class JsonGeneratorEngine {
     	System.out.println(randomStringToMatch + " FROM PATTERN " + pattern.toString() );// a random value from the previous String list
     }
 
-    void visitCombinedSchema(CombinedSchema combinedSchema) {
-    	ValidationCriterion criterion = combinedSchema.getCriterion();
-		System.out.println("criterion: " +criterion);
-		Collection<Schema> subschemas = combinedSchema.getSubschemas();
-		System.out.println(subschemas.size());
-		
-		if (criterion == CombinedSchema.ALL_CRITERION) {
-			for (Schema subschema : subschemas) {
-				System.out.print(subschema.getClass() + " ");
-				if (subschema instanceof EnumSchema) {
-					visit(subschema);
-				}
-			}
-		} else {
-			visit(subschemas.iterator().next());
-		}
-    }
-
     void visitConditionalSchema(ConditionalSchema conditionalSchema) {
         conditionalSchema.getIfSchema().ifPresent(this::visitIfSchema);
         conditionalSchema.getThenSchema().ifPresent(this::visitThenSchema);
@@ -372,36 +357,5 @@ public class JsonGeneratorEngine {
 
     void visitNotSchema(NotSchema notSchema) {
     }
-    
-    void visitMultipleOf(Number multipleOf) {
-    }
-    
-    /*
-     * 	static void visitObjectSchema(ObjectSchema objectSchema) {
-        for (String requiredPropName : objectSchema.getRequiredProperties()) {
-            System.out.println("requiredPropName " + requiredPropName);
-           
-        }
-        System.out.println(objectSchema.getPropertySchemas());
-        System.out.println(objectSchema.getPropertyNameSchema());
-        System.out.println(objectSchema.getMinProperties());
-        System.out.println(objectSchema.getMaxProperties());
-        for (Map.Entry<String, Set<String>> entry : objectSchema.getPropertyDependencies().entrySet()) {
-        	System.out.println(entry.getKey() + entry.getValue());
-        }
-        System.out.println(objectSchema.permitsAdditionalProperties());
-        System.out.println(objectSchema.getSchemaOfAdditionalProperties());
-        
-        // TODO 
-        for (Entry<Pattern, Schema> entry : objectSchema.getPatternProperties().entrySet()) {
-            //visitPatternPropertySchema(entry.getKey(), entry.getValue());
-        	System.out.println("pattern: " + entry.getKey() + " schema: " + entry.getValue());
-        }
-        
- //       for (Map.Entry<String, Schema> schemaDep : objectSchema.getSchemaDependencies().entrySet()) {
- //       }
-        
-    }
-     */
 
 }
